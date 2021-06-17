50d8b44872aa61274d208e3f9543dba9702211af
Minsu Kwon <minsu@publy.co>
2021-06-17 15:27:58 +0900

    add session prop to provider

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import './styles.css'
 export default function App ({ Component, pageProps }) {
   return (
     <Provider
+      session={pageProps.session}
       // Provider options are not required but can be useful in situations where
       // you have a short session maxAge time. Shown here with default values.
       options={{
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Layout from '../components/layout'
+import { getSession } from 'next-auth/client';
 
-export default function Page () {
+export default function Page ({ session }) {
+  console.log(session);
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
@@ -9,4 +11,12 @@ export default function Page () {
       </p>
     </Layout>
   )
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      session: await getSession(context)
+    }
+  }
 }
